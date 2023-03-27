11f9acb0c2daf9d410650059ad114ad5c3be4221
Facu Falcone <a.facundo.falcone@gmail.com>
2023-03-27 18:39:42 -0300

    ♻️ Upg - Upgrade code & comments

--- TP_01/TP1_GRUPO_3/src/ejercicio1/Empleado.java
+++ TP_01/TP1_GRUPO_3/src/ejercicio1/Empleado.java
@@ -1,32 +1,77 @@
+/**
+ * Copyright (C) 2023 <Grupo 3 - UTN FRGP>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ejercicio1;
 
 public class Empleado {
 
-	//ATTRIBUTES
+	// ATTRIBUTES
 	
 	private int id;
 	private String nombre;
 	private int edad;
 	
-	//GETTERS AND SETTERS
+	// GETTERS AND SETTERS
+	
+	/**
+	 * Gets the employee ID
+	 * @return the employee ID as an integer.
+	 */
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
+	
+	/**
+	 * Gets the employee name.
+	 * @return The employee name as a string.
+	 */
 	public String getNombre() {
 		return nombre;
 	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+
+	/**
+	 * Gets the employee age.
+	 * @return The employee age as an integer.
+	 */
 	public int getEdad() {
 		return edad;
 	}
+
+	/**
+	 * Sets the employee ID
+	 * @param id The id of the given employee. Must be an integer. 
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Sets the employee name.
+	 * @param nombre The employee name as a string.
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	/**
+	 * Sets the employee age.
+	 * @param edad The age of the employee, as an integer.
+	 */
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-	
-	
 }
