8bc7b643bb182878b66e5d156ca6bdccaa0cdf19
Andylisco <57235735+Andylisco@users.noreply.github.com>
2023-03-27 18:03:09 -0300

    Clase Empleado
    Se genero la clase empleado y se encapsulo. Sus atributos con getters and setters

---
+++ TP_01/TP1_GRUPO_3/src/ejercicio1/Empleado.java
@@ -0,0 +1,32 @@
+package ejercicio1;
+
+public class Empleado {
+
+	//ATTRIBUTES
+	
+	private int id;
+	private String nombre;
+	private int edad;
+	
+	//GETTERS AND SETTERS
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+}
