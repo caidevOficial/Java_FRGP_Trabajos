cd0370ed2af400b09cfeb59eb32b4a96ea48e668
unknown <saibot_iaconis99@hotmail.com>
2023-04-12 17:03:21 -0300

    Se termina la clase InstDeportiva

--- TP_02/src/ejercicio3/InstDeportiva.java
+++ TP_02/src/ejercicio3/InstDeportiva.java
@@ -3,16 +3,27 @@ package ejercicio3;
 public class InstDeportiva {
 	///atributos
 	
+	private final int ID;
 	private String nombre;
+	private int TipoDeInstalacion;
+	
+	//ATRIBUTOS DE LA CLASE
+	static int cont = 0;
+
 	
 	///constructores
 	public InstDeportiva(){
-		
+		this.ID = cont;
 		this.nombre = "Sin nombre";
+		this.TipoDeInstalacion = 0;
+		cont++;
 	}
-	public InstDeportiva(String nombre){
+	public InstDeportiva(String nombre, int TipoDeInstalacion){
 		
 		this.nombre = nombre;
+		this.ID = cont;
+		this.TipoDeInstalacion = TipoDeInstalacion;
+		cont++;
 	}
 	
 	///getters y  setters
@@ -23,13 +34,28 @@ public class InstDeportiva {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	public int getID() {
+		return ID;
+	}
+//	public void setID(int iD) {
+//		ID = iD;
+//	}
+	public int getTipoDeInstalacion() {
+		return TipoDeInstalacion;
+	}
+	public void setTipoDeInstalacion(int tipoDeInstalacion) {
+		TipoDeInstalacion = tipoDeInstalacion;
+	}
 	///toString
-
+	
 	@Override
 	public String toString() {
-		return "La institucion se llama" + nombre;
+		return "InstDeportiva ID=" + ID + ", El nombre de la instalacion es= " + nombre + ", Su tipo de instalacion=" + TipoDeInstalacion;
 	}
+		
 	
+
 	
 	
 
