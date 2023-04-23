38a316e4c9bc1a35d2dd3cb9303c20a714a7d1a5
mateokrasevec <mekrasevec@gmail.com>
2023-04-23 20:47:33 -0300

    Dejo el trabajo con el main desarrollado y con el archivo Resultado creado. Falta hacer que escriba los items en dicho archivo

--- TP_03/src/ejercicio1/Main.java
+++ TP_03/src/ejercicio1/Main.java
@@ -2,17 +2,24 @@ package ejercicio1;
 
 import java.util.TreeSet;
 
+
 public class Main 
 {
+	final static String ruta = "Java_FRGP_Trabajos\\TP_03";
 	
 	public static void main(String[] args) throws DniInvalido {
-
+		
+		Archivo Resultado = new Archivo();
+		Resultado.setRuta(ruta);
+		
+		
 		Persona aux = new Persona();
 		TreeSet<Persona> ListaPersona = new TreeSet<Persona>();
 		ListaPersona = aux.LeetDatos();
 		
 		for(Persona item : ListaPersona) {
 			System.out.println(item.toString());
+			
 		}
 	}
 }
