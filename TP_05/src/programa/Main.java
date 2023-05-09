d726361e2d42ad0597ef754e819cc920dae308cb
Andylisco <57235735+Andylisco@users.noreply.github.com>
2023-05-09 10:15:10 -0300

    Primer Aporte
    Cree las clases la ventana principal con le menú y la ventana agregar al dar clic en el menú agregar nos lleva al panel de agregar, donde pone como id la cantidad que hay en el LTModel + 1 , también se agregar el listado de categorías al combo box.

---
+++ TP_05/src/programa/Main.java
@@ -0,0 +1,23 @@
+package programa;
+
+import java.awt.EventQueue;
+
+public class Main {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ventana frame = new Ventana();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
---
+++ TP_05/src/programa/Pnl_Agregar.java
@@ -0,0 +1,65 @@
+package programa;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import dominio.Categorias;
+
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+
+public class Pnl_Agregar extends JPanel {
+	private JTextField txt_Nombre;
+	private JLabel lblNewLabel;
+	private  JLabel lbl_ID ;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JComboBox<Categorias> comboBox;
+	private JButton btn_Aceptar;
+	
+	private int CantidadRegistrosActual;
+	
+		/**
+	 * Create the panel.
+	 */
+	public Pnl_Agregar(int ultimoID) {
+		setLayout(null);
+		
+		lblNewLabel = new JLabel("ID");
+		lblNewLabel.setBounds(61, 43, 46, 14);
+		add(lblNewLabel);
+		
+		lbl_ID = new JLabel(String.valueOf(ultimoID));
+		lbl_ID.setBounds(183, 43, 46, 14);
+		add(lbl_ID);
+		
+		lblNewLabel_1 = new JLabel("Nombre");
+		lblNewLabel_1.setBounds(61, 97, 46, 14);
+		add(lblNewLabel_1);
+		
+		txt_Nombre = new JTextField();
+		txt_Nombre.setBounds(183, 94, 146, 20);
+		add(txt_Nombre);
+		txt_Nombre.setColumns(10);
+		
+		lblNewLabel_2 = new JLabel("Genero");
+		lblNewLabel_2.setBounds(61, 162, 46, 14);
+		add(lblNewLabel_2);
+		
+		comboBox = new JComboBox<Categorias>();
+		comboBox.setBounds(183, 159, 146, 20);
+		add(comboBox);
+		comboBox.addItem(new Categorias("Seleccione un genero"));
+		comboBox.addItem(new Categorias("Terror"));
+		comboBox.addItem(new Categorias("Accion"));
+		comboBox.addItem(new Categorias("Suspeso"));
+		comboBox.addItem(new Categorias("Romantica"));
+		
+		btn_Aceptar = new JButton("Aceptar");
+		btn_Aceptar.setBounds(61, 219, 118, 23);
+		add(btn_Aceptar);
+
+	}
+
+}
